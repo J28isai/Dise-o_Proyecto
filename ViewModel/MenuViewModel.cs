bcdcea0e0e045e9cdf9885af893b26cb32aad937
Isai Ferrufino <isai26361@gmail.com>
2026-03-09 00:00:04 -0600

    a

--- ViewModel/MenuViewModel.cs
+++ ViewModel/MenuViewModel.cs
@@ -1,11 +1,6 @@
 ﻿using Diseño_Proyecto.Model;
 using Diseño_Proyecto.Repository;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
